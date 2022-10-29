7a464a424b12e46dc650a63a307ee84dba145892
Rahman Islam <54945042+RahmanIslamIen@users.noreply.github.com>
2022-10-29 18:46:18 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// Importing module
+const chalk = require("chalk");
+
+console.log(
+  chalk`{bgGray Selamat Datang di }{bgBlue -= caffe mantap node.js rahman =-}`
+);
+
+// const readline = require("node:readline");
+// const { stdin: input, stdout: output } = require("node:process");
+
+// const rl = readline.createInterface({ input, output });
+
+// rl.question("What do you think of Node.js? ", (answer) => {
+//   // TODO: Log the answer in a database
+//   console.log(`Thank you for your valuable feedback: ${answer}`);
+
+//   rl.close();
+// });
