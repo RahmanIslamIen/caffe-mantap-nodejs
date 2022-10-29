f19f0195bd772e10dbf2162cac38a52dc543672a
Rahman Islam <54945042+RahmanIslamIen@users.noreply.github.com>
2022-10-29 18:59:57 +0700

    penambahan chalk dan fitur nama

--- app.js
+++ app.js
@@ -5,14 +5,13 @@ console.log(
   chalk`{bgGray Selamat Datang di }{bgBlue -= caffe mantap node.js rahman =-}`
 );
 
-// const readline = require("node:readline");
-// const { stdin: input, stdout: output } = require("node:process");
+const readline = require("node:readline");
+const { stdin: input, stdout: output } = require("node:process");
 
-// const rl = readline.createInterface({ input, output });
+const rl = readline.createInterface({ input, output });
 
-// rl.question("What do you think of Node.js? ", (answer) => {
-//   // TODO: Log the answer in a database
-//   console.log(`Thank you for your valuable feedback: ${answer}`);
+rl.question("Masukan Nama Anda : ", (nama) => {
+  console.log(chalk`Silahkan Pesan {underline ${nama}}`);
 
-//   rl.close();
-// });
+  rl.close();
+});
